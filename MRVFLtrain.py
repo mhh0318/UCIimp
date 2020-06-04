663e48512fad9c20c8161149b43fa18a296a14f7
mmh0318 <mercihu0318@gmail.com>
2020-06-04 21:54:34 +0800

    aFS-edRVFL

--- MRVFLtrain.py
+++ MRVFLtrain.py
@@ -4,7 +4,8 @@ from function import *
 from l2_weights import *
 from majorityVoting import *
 from model import model as mod
-
+        elif mode == 'append':
+            w = s * 2 * cp.asarray(rand_seed.rand(n_dims + selected_amount - drop_amount + N, N)) - 1
 
 def MRVFLtrain(trainX, trainY, option):
     # mode = 'append'
